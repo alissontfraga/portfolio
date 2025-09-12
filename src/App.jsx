99bc380885a11b00c2c99c7829a42eba63b23cd5
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-09-12 09:14:27 -0300

    style: add navbar and home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import './styles/App.scss'
+import './styles/_Theme.scss'
+import Background from './components/Background/Background'
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home'
 
-import './App.css'
+
 
 function App() {
 
 
   return (
     <>
+      <Background />
+      <Navbar />
+      <Home />
       
     </>
   )
