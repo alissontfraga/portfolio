fcf27d1564d58ef7fffbb524022c59168812c385
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-09-12 09:55:35 -0300

    style: add about component

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './About.scss'
+
+const About = () => {
+  return (
+    <div className='about-container'>
+              <div className='about-text'>
+                  <h2 className='about-title'>About me</h2>
+                  <p>I've been studying software engineering since December 2024.
+                     I have basic knowledge of React, JavaScript, Java, C, C++, CSS, HTML, SQL, Git, Github, AWS Cloud, and intermediate English.</p>
+                </div>
+        
+    </div>
+  )
+}
+
+export default About
